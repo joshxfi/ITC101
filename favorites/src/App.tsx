314728f47e47c04d112346d55d909b9c667a1f7b
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-16 15:02:33 +0800

    feat: add song card

--- favorites/src/App.tsx
+++ favorites/src/App.tsx
@@ -1,7 +1,27 @@
+import { BsPlayCircleFill } from "react-icons/bs";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bg-stone-800 min-h-screen text-white flex flex-col items-center justify-center">
+      <section className="bg-stone-900 p-8 rounded-xl w-[350px] shadow-lg">
+        <img
+          src="/songs/10.jpeg"
+          alt="as it was"
+          className="w-full h-[200px] object-cover rounded-lg"
+        />
+
+        <div className="mt-8 flex justify-between items-center">
+          <div className="flex space-x-4 items-center">
+            <h1 className="text-green-500 text-6xl font-extrabold">10</h1>
+            <div>
+              <h2 className="font-bold text-2xl">Matilda</h2>
+              <h3 className="text-stone-500 font-medium text-sm">Harry Styles</h3>
+            </div>
+          </div>
+
+          <BsPlayCircleFill className='text-5xl hover:text-green-400 hover:scale-110 transition-all inline-block cursor-pointer' />
+        </div>
+      </section>
     </div>
   );
 }
