588c471a45087a6468cade3f95f315e118fe802c
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-17 21:51:47 +0800

    feat: display top songs

--- favorites/src/App.tsx
+++ favorites/src/App.tsx
@@ -1,22 +1,22 @@
 import { Song } from "./components";
+import { topSongs } from "./constants";
 
 function App() {
   return (
-    <div className="bg-stone-800 min-h-screen text-white flex gap-4 items-center justify-center">
-      <Song
-        top={10}
-        title="Matilda"
-        author="Harry Styles"
-        image="matilda.jpeg"
-        link="#"
-      />
-      <Song
-        top={9}
-        title="Hello Anxiety"
-        author="Phum Viphurit"
-        image="hello_anxiety.jpg"
-        link="#"
-      />
+    <div className="bg-stone-800 min-h-screen text-white grid place-items-center">
+      <div className='grid grid-cols-3 gap-6'>
+
+      {topSongs.map((s) => (
+        <Song
+          key={s.title}
+          top={s.top}
+          title={s.title}
+          artist={s.artist}
+          image={s.img}
+          link={s.link}
+        />
+      ))}
+      </div>
     </div>
   );
 }
--- favorites/src/components/Song.tsx
+++ favorites/src/components/Song.tsx
@@ -3,26 +3,28 @@ import { BsPlayCircleFill } from "react-icons/bs";
 interface Props {
   top: number;
   title: string;
-  author: string;
+  artist: string;
   image: string;
   link: string;
 }
 
-export const Song = ({ top, title, author, image, link }: Props) => {
+export const Song = ({ top, title, artist, image, link }: Props) => {
   return (
-    <section className="bg-stone-900 p-8 rounded-xl w-[400px] shadow-lg">
-      <img
-        src={`/songs/${image}`}
-        alt="as it was"
-        className="w-full h-[200px] object-cover rounded-lg"
-      />
+    <section className="group bg-stone-900 p-8 rounded-xl w-[400px] shadow-lg">
+      <div className="overflow-hidden transition-all">
+        <img
+          src={`/songs/${image}`}
+          alt={title}
+          className="w-full h-[200px] grayscale hover:grayscale-0 transition-all object-cover rounded-lg object-top group-hover:scale-110 "
+        />
+      </div>
 
       <div className="mt-8 flex justify-between items-center">
         <div className="flex space-x-4 items-center">
           <h1 className="text-green-500 text-6xl font-extrabold">{top}</h1>
           <div>
             <h2 className="font-bold text-2xl">{title}</h2>
-            <h3 className="text-stone-500 font-medium text-sm">{author}</h3>
+            <h3 className="text-stone-500 font-medium text-sm">{artist}</h3>
           </div>
         </div>
 
@@ -30,7 +32,7 @@ export const Song = ({ top, title, author, image, link }: Props) => {
           href={link}
           target="_blank"
           rel="noopener noreferrer"
-          className="peer text-5xl hover:text-green-400 hover:scale-110 transition-all inline-block cursor-pointer"
+          className="peer text-5xl hover:text-green-500 transition-colors inline-block cursor-pointer"
         >
           <BsPlayCircleFill />
         </a>
