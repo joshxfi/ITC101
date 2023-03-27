649b764897e6fd6e87b045ddcf85f7751a532850
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-27 22:14:30 +0800

    feat: add navbar

--- its-website/src/components/Layout.tsx
+++ its-website/src/components/Layout.tsx
@@ -1,6 +1,11 @@
+import { Navbar } from "./Navbar";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen 2xl:w-full w-[90%] mx-auto max-w-screen-2xl text-white">
+    <div className={`min-h-screen 2xl:w-full w-[90%] mx-auto max-w-screen-xl text-white ${inter.className}`}>
+      <Navbar />
       {children}
     </div>
   );
---
+++ its-website/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center mt-12">
+      <h1 className='text-green-600 font-black text-5xl'>ITS</h1>
+
+      <div className="flex space-x-24 items-center">
+        <ul className="flex space-x-12">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+
+        <button type="button" className="bg-green-600 font-medium rounded px-8 py-3">
+          Register
+        </button>
+      </div>
+    </nav>
+  );
+};
