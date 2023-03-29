1ea63e06384a91ece6bb536126717e314c2da6b6
Josh Daniel <joshxfi.dev@gmail.com>
2023-03-29 17:02:26 +0800

    feat: add intro animation

--- its-website/src/components/Hero.tsx
+++ its-website/src/components/Hero.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Gruppo } from 'next/font/google';
 
 import { Button, Container } from '@/components';
+import { motion } from 'framer-motion';
 
 const gruppo = Gruppo({ weight: ['400'], subsets: ['latin'] });
 
@@ -9,14 +10,39 @@ export const Hero = () => {
   return (
     <Container className='flex justify-between items-center flex-col xl:flex-row'>
       <div className='text-center xl:text-left'>
-        <h1 className={`md:text-7xl text-4xl ${gruppo.className}`}>It Starts with ITS</h1>
-        <p className='md:max-w-[650px] max-w-[400px] mt-4 md:text-lg text-sm font-light'>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className={`md:text-7xl text-4xl ${gruppo.className}`}
+        >
+          It Starts with ITS
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            delay: 1,
+            duration: 1,
+          }}
+          className='md:max-w-[650px] max-w-[400px] mt-4 md:text-lg text-sm font-light'
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
           ducimus, veniam nam fugiat sed magnam id? Exercitationem
           necessitatibus soluta ut.
-        </p>
+        </motion.p>
 
-        <div className='flex space-x-3 mt-8 justify-center xl:justify-start text-sm md:text-base'>
+        <motion.div
+          initial={{ x: -500, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            delay: 1.5,
+            duration: 1,
+            type: 'spring',
+            stiffness: 100,
+          }}
+          className='flex space-x-3 mt-8 justify-center xl:justify-start text-sm md:text-base'
+        >
           <Button text='Get Started' />
 
           <button
@@ -25,10 +51,17 @@ export const Hero = () => {
           >
             Learn More &rarr;
           </button>
-        </div>
+        </motion.div>
       </div>
 
-        <div className='rounded-full relative bg-secondary-400 w-[400px] h-[400px] hidden xl:block overflow-hidden'>
+      <div className='bg-secondary-400 w-[400px] h-[400px] hidden xl:block overflow-hidden rounded-full'>
+        <motion.div
+          initial={{ y: 300 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 1, type: 'spring', stiffness: 100 }}
+          whileTap={{ scale: 0.9 }}
+          className='relative w-[400px] h-[400px] cursor-pointer'
+        >
           <Image
             src='/assets/avatar.png'
             className='object-contain mt-12 ml-4'
@@ -36,7 +69,8 @@ export const Hero = () => {
             sizes='(max-width: 768px) 100vw, 400px'
             fill
           />
-        </div>
+        </motion.div>
+      </div>
     </Container>
   );
 };
